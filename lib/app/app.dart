c7f30dd1039754cd1a7f7f0f9d6b4c723fd4b04f
ayushkarani47 <ayushkarani4747@gmail.com>
2025-01-18 12:14:33 +0530

    added app and controller

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,18 @@
+import 'package:Fitnessio/controller/language_change_controller.dart';
+import 'package:Fitnessio/trainer/auth/provider/auth_provider_trainer.dart';
+import 'package:Fitnessio/trainer/home/provider/trainer_home_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-import 'package:smart_home_app/presentation/auth/providers/auth_provider.dart';
-import 'package:smart_home_app/presentation/consumption/providers/consumption_provider.dart';
-import 'package:smart_home_app/presentation/home/providers/home_provider.dart';
-import 'package:smart_home_app/presentation/profile/providers/profile_provider.dart';
-import 'package:smart_home_app/presentation/settings/providers/settings_provider.dart';
-import 'package:smart_home_app/presentation/workouts/providers/workout_provider.dart';
-import 'package:smart_home_app/utils/router/router.dart';
+import 'package:Fitnessio/presentation/auth/providers/auth_provider.dart';
+import 'package:Fitnessio/presentation/consumption/providers/consumption_provider.dart';
+import 'package:Fitnessio/presentation/home/providers/home_provider.dart';
+import 'package:Fitnessio/presentation/profile/providers/profile_provider.dart';
+import 'package:Fitnessio/presentation/settings/providers/settings_provider.dart';
+import 'package:Fitnessio/presentation/workouts/providers/workout_provider.dart';
+import 'package:Fitnessio/utils/router/router.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 // ignore: must_be_immutable
 class MyApp extends StatefulWidget {
@@ -27,6 +32,12 @@ class _MyAppState extends State<MyApp> {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider.value(
+          value: TrainerHomeProvider(),
+        ),
+        ChangeNotifierProvider.value(
+          value: AuthProviderTrainer(),
+        ),
+        ChangeNotifierProvider.value(
           value: AuthProvider(),
         ),
         ChangeNotifierProvider.value(
@@ -43,14 +54,34 @@ class _MyAppState extends State<MyApp> {
         ),
         ChangeNotifierProvider.value(
           value: ProfileProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => LanguageChangeController(),
         )
       ],
       child: ScreenUtilInit(
-        builder: (context, child) => const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          onGenerateRoute: RouteGenerator.getRoute,
-          initialRoute: Routes.boardingRoute,
-        ),
+        builder: (context, child) => Consumer<LanguageChangeController>(
+            builder: (context, provider, child) {
+          print("hello");
+          
+          print(provider.appLocale);
+          return MaterialApp(
+            locale: provider.appLocale,
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate
+            ],
+            supportedLocales: const [
+              Locale('en'),
+              Locale('nl'),
+            ],
+            debugShowCheckedModeBanner: false,
+            onGenerateRoute: RouteGenerator.getRoute,
+            initialRoute: Routes.boardingRoute,
+          );
+        }),
         designSize: const Size(430, 810),
       ),
     );
